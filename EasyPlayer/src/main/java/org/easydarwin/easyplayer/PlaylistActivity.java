638c5a39766de974ba25ffdfd5fcebc274c63be3
kim <ahqjjl@163.com>
2018-11-14 22:46:03 +0800

    修复部分视频源播放非常卡顿的问题

--- EasyPlayer/src/main/java/org/easydarwin/easyplayer/PlaylistActivity.java
+++ EasyPlayer/src/main/java/org/easydarwin/easyplayer/PlaylistActivity.java
@@ -75,7 +75,7 @@ public class PlaylistActivity extends AppCompatActivity implements View.OnClickL
         mCursor = TheApp.sDB.query(VideoSource.TABLE_NAME, null, null, null, null, null, null);
         if (!mCursor.moveToFirst()){
             ContentValues cv = new ContentValues();
-            cv.put(VideoSource.URL, "rtmp://live.hkstv.hk.lxdns.com/live/hks");
+            cv.put(VideoSource.URL, "rtmp://live.hkstv.hk.lxdns.com/live/hks2");
             TheApp.sDB.insert(VideoSource.TABLE_NAME, null, cv);
             mCursor.close();
             mCursor = TheApp.sDB.query(VideoSource.TABLE_NAME, null, null, null, null, null, null);
